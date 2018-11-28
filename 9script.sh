bf780cd8410c1c306acb7d70ceb2dac25f809342
Suman <sthasum5@gmail.com>
2018-11-28 17:10:46 -0500

    calculations

---
+++ 9script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+num1=20
+num2=5
+
+
+echo $(( num1 + num2 ))
+echo $(( num1 - num2 ))
+echo $(( num1 / num2 ))
+echo $(( num1 * num2 ))
+echo $(( num1 % num2 ))
+echo $(( num1 - num2 ))
